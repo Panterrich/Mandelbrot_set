1b090697006bbb35e0a6b96506c471838ab436ba
Panterrich <durnov.an@phystech.edu>
2021-03-26 15:08:13 +0300

    SSE128

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 
 const int Nmax = 10000;
 
-const __m128  Steps = _mm_set_ps(3, 2, 1, 0);
+const __m128 Steps = _mm_set_ps(3, 2, 1, 0);
+const __m128 Mask  = _mm_set_ps1(1);
 
 //==================================================================
 
@@ -56,10 +57,33 @@ void Set_text(sf::Text* text, const sf::Font& font, const sf::Color color, size_
 
 void Set_pixel(sf::Uint8* pixels, int dx, int dy, int n)
 {
-    pixels[(1280 * dy + dx) * 4 + 0] = 5 * tanh (n);
-    pixels[(1280 * dy + dx) * 4 + 1] = sinh (n);
-    pixels[(1280 * dy + dx) * 4 + 2] = 255 - sin (n) * 12 * cosh(n);
-    pixels[(1280 * dy + dx) * 4 + 3] = 255 - exp (-(float)n / 255);
+    // pixels[(1280 * dy + dx) * 4 + 0] = 5 * tanh (n);
+    // pixels[(1280 * dy + dx) * 4 + 1] = sinh (n);
+    // pixels[(1280 * dy + dx) * 4 + 2] = 255 - sin (n) * 12 * cosh(n);
+    // pixels[(1280 * dy + dx) * 4 + 3] = 255 - exp (-(float)n / 255);
+
+    // pixels[(1280 * dy + dx) * 4 + 0] = 5 * n;
+    // pixels[(1280 * dy + dx) * 4 + 1] = n;
+    // pixels[(1280 * dy + dx) * 4 + 2] = 255 - 12 * n;
+    // pixels[(1280 * dy + dx) * 4 + 3] = 255;
+
+    if (n == Nmax)
+    {
+        pixels[(1280 * dy + dx) * 4 + 0] = 0;
+        pixels[(1280 * dy + dx) * 4 + 1] = 0;
+        pixels[(1280 * dy + dx) * 4 + 2] = 0;
+        pixels[(1280 * dy + dx) * 4 + 3] = 0;
+    }
+    
+    else
+    {
+        pixels[(1280 * dy + dx) * 4 + 0] = 100 * (1 + sin(0.5 * n + 0));
+        pixels[(1280 * dy + dx) * 4 + 1] = 100 * (1 + sin(0.5 * n + 2));
+        pixels[(1280 * dy + dx) * 4 + 2] = 100 * (1 + sin(0.5 * n + 4));
+        pixels[(1280 * dy + dx) * 4 + 3] = 255;
+    }
+    
+
 }
 
 void Count_mondelbrot_set(sf::Uint8* pixels, float scale, float cx, float cy, float R)
@@ -78,7 +102,13 @@ void Count_mondelbrot_set(sf::Uint8* pixels, float scale, float cx, float cy, fl
         {
             __m128 X0 = _mm_add_ps(_mm_set_ps1(x0), _mm_mul_ps(Scale, Steps));
             __m128 Y0 = _mm_set_ps1(y0);
-            __m128i N = _mm_setzero_si128();
+
+            union 
+            {
+                int iterations[4];
+                __m128i N = _mm_setzero_si128();
+            };
+            
 
             int n = 0;
 
@@ -93,15 +123,19 @@ void Count_mondelbrot_set(sf::Uint8* pixels, float scale, float cx, float cy, fl
 
                 __m128 r2 = _mm_add_ps(x2, y2);
 
-                if (r2 > R2) break;
+                __m128 inc = _mm_cmplt_ps(r2, R2);
+                if (!_mm_movemask_ps(inc)) break;
 
                 X = _mm_add_ps(_mm_sub_ps(x2, y2), X0);
                 Y = _mm_add_ps(_mm_add_ps(xy, xy), Y0);
+
+                N = _mm_add_epi32(_mm_cvtps_epi32(_mm_and_ps(inc, Mask)), N);
+                
             }
             
-            for (int i = 0; i < 4; ++i)
-            {
-                Set_pixel(pixels, dx + i, dy, N[i]);
+            for (int i = 0; i < 4; i++)
+            {   
+                Set_pixel(pixels, dx + i, dy, iterations[i]);
             }
         }
 
@@ -131,7 +165,8 @@ int main()
     int x = 760;
     int y = 360;
 
-    bool box_fps = false;
+    bool draw_set = false;
+    bool box_fps  = false;
     
     while (window.isOpen())
     {
@@ -160,15 +195,26 @@ int main()
                         }
                         break;
 
+                        case sf::Keyboard::Escape:
+                        {
+                            draw_set = !draw_set;
+                        }
+                        break;
+
                         case sf::Keyboard::Add:
                         {
-                            scale *= 0.75;
+                            scale *= 0.8;
+                            x *= 1.25;
+                            y *= 1.25;
+                           
                         }
                         break;
 
                         case sf::Keyboard::Subtract:
                         {
                             scale *= 1.25;
+                            x *= 0.8;
+                            y *= 0.8;
                         }
                         break;
 
@@ -186,13 +232,13 @@ int main()
 
                         case sf::Keyboard::Right:
                         {
-                            x += 100;
+                            x -= 100;
                         }
                         break;
 
                         case sf::Keyboard::Left:
                         {
-                            x -= 100;
+                            x += 100;
                         }
                         break;
 
@@ -205,7 +251,11 @@ int main()
             }
         }
 
-        Count_mondelbrot_set(pixels, scale, x, y, 100.f);
+        if (draw_set) 
+        {
+            Count_mondelbrot_set(pixels, scale, x, y, 100.f);
+        }
+
         screen.update(pixels);
         
 
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	g++ main.cpp -o set -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+	g++ main.cpp -o set -O3 -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
